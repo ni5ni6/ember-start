84bae520df8762efc5e6e552793b0b0ed7c58976
unknown <ni5ni6@gmail.com>
2013-03-23 14:39:37 +0100

    Adds Index route and index template

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ App.ApplicationController = Ember.Controller.extend({
     firstName: "Trek",
     lastName: "Glowacki"
 });
+
+App.IndexRoute = Ember.Route.extend({
+    setupController: function (controller) {
+        controller.set('title', "My App Index");
+    }
+});
